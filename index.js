039857483211f34e0954b9b112a79cce7b395cdb
Lorel <lorel.oboue@gmail.com>
2022-08-05 10:04:20 +0100

    update

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // REQUIRE ROUTERS
-const usersRouter = require("./src/routers/users");
+// const usersRouter = require("./src/routers/users");
 
 // ADD ROUTERS TO APP
 
+const { books, films, users } = require("./data.js");
+
+//Route/endpoint for get a user by ID
+app.get("/users/:id", (req, res) => {
+  // console.log("we're working fine over here!");
+  // console.log(users);
+
+  const userFound = users.find((user) => user.id === Number(req.params.id));
+
+  if (!userFound) {
+    return res.status(404).json("A user with the provided ID does not exist");
+  }
+
+  res.json({
+    user: userFound,
+  });
+});
 
 /* START SERVER */
 app.listen(port, () => {
